d67702f0b48e39fb1d0c756bad71637f3c1d1275
LZGabriel1409 <lg.moraes.lima@gmail.com>
2026-03-17 09:35:55 -0300

    Add files via upload

---
+++ src/Calculadora.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Calculadora {
+    private JFrame janela;
+
+    public Calculadora(){
+        janela = new JFrame();
+
+        janela.setSize(350, 450);
+        janela.setTitle("Calculadora");
+
+        janela.setVisible(true);
+    }
+}
